be47c9b661dfccdc48d21ffee3f0d7f43f3da990
DayMoniakk <54359117+DayMoniakk@users.noreply.github.com>
2024-06-05 14:15:38 +0200

    Removed unneeded cache folder

--- .netlify/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.Utm0XptD.js","_app/immutable/chunks/scheduler.dM4H8AQ0.js","_app/immutable/chunks/index.ArIGaIU1.js","_app/immutable/chunks/entry.yq9oOVfB.js"];
-export const stylesheets = [];
-export const fonts = [];
